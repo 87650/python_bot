18996c22f1a95faca60b5084e0923f959065c388
Sasha <toma6456@gmail.com>
2021-10-11 12:57:52 +0300

    Transferring functionality to another file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import discord
 import datetime
 from discord.ext import commands
-from mysql.connector import connect, Error
+import sqls
 
 from discord.message import *
 class Bot(discord.Client):
-    token_bot = ""
+    token_bot = "ODUzMjY0NjAxNTQxOTAyMzQ2.YMS2lQ.IbqZf0-_4K5gsreLWL0Ld7qeE0U"
 
     async def on_ready(self):
 
@@ -13,16 +13,8 @@ class Bot(discord.Client):
             if not hasattr(bot, 'appinfo'):
                bot.appinfo = await bot.application_info()
                self.bot = bot
-            try:
-                self.connection = connect(
-                        host="localhost",
-                        user="",
-                        password="",
-                        database="task",)
+               self.sql = sqls.Sql()
 
-
-            except Error as e:
-                print(e)
 
 
     async def on_message(self,message):
@@ -36,14 +28,14 @@ class Bot(discord.Client):
 
             elif(message.content.find("!enter notes") != -1):
 
-                await self.getsql(message)
+                await self.sql.getsql(message,discord,self.bot)
             elif (message.content.find("!display notes") != -1):
 
-                await self.setsql(message)
+                await self.sql.setsql(message,discord,self.bot)
             elif(message.content.find("!delete notes") != -1):
-                await self.deletesql(message)
+                await self.sql.deletesql(message,discord,self.bot)
             elif (message.content.find("!update notes") != -1):
-                await self.updatesql(message)
+                await self.sql.updatesql(message,discord,self.bot)
 
 
     async def cached(self,message):
@@ -54,67 +46,7 @@ class Bot(discord.Client):
             for channal in self.get_all_channels():
                 await message.channel.send(channal)
 
-    async def getsql(self,message):
-        mes = message.content.replace("!enter notes", " ")
-        with self.connection.cursor() as cursor:
 
-              cursor.execute("SELECT id FROM bot where name = '{0}'".format(self.bot.appinfo.owner))
-              ready_id = 0
-              sql_test = cursor.fetchall()
-              for sql_value in sql_test:
-                  ready_id = sql_value
-
-              if ready_id == 0:
-                    cursor.execute("INSERT INTO bot (name,message) VALUES ('{0}','{1}');".format(self.bot.appinfo.owner,mes))
-                    self.connection.commit()
-                    await message.channel.send("ваша заметка создана - {0} ".format(mes))
-              else:
-                  await message.channel.send("у вас уже есть заметки, чтобы их вывести введите - !display notes")
-
-    async def setsql(self,message):
-        #mes = message.content.replace("!display notes", " ")
-        with self.connection.cursor() as cursor:
-            cursor.execute("SELECT name,message,date_create FROM bot where name = '{0}'".format(self.bot.appinfo.owner))
-            sql_test = cursor.fetchall()
-            if not sql_test:
-                await message.channel.send("у вас нету заметок")
-            else:
-
-                embed = discord.Embed(color=0x7daeff)
-                embed.add_field(name='By', value=sql_test[0][0], inline=True)
-                embed.add_field(name='Time', value=sql_test[0][2], inline=True)
-                embed.add_field(name='Message content', value=sql_test[0][1], inline=False)
-                await message.channel.send(self.bot.appinfo.owner, embed=embed)
-
-
-
-    async def deletesql(self,message):
-        with self.connection.cursor() as cursor:
-            cursor.execute("DELETE FROM bot WHERE name = '{0}'".format(self.bot.appinfo.owner))
-            self.connection.commit()
-            await message.channel.send("Заметки удалены!")
-    async def updatesql(self,message):
-        mes = message.content.replace("!update notes", " ")
-        with self.connection.cursor() as cursor:
-            cursor.execute("SELECT name,message,date_create FROM bot where name = '{0}'".format(self.bot.appinfo.owner))
-            sql_test = cursor.fetchall()
-            if not sql_test:
-                await message.channel.send("у вас нету заметок для обновления")
-            else:
-
-                embed = discord.Embed(color=0x7daeff)
-                embed.add_field(name='By', value=sql_test[0][0], inline=True)
-                embed.add_field(name='Time', value=sql_test[0][2], inline=True)
-                embed.add_field(name='Message content', value=sql_test[0][1], inline=False)
-                await message.channel.send(self.bot.appinfo.owner, embed=embed)
-                cursor.execute("UPDATE bot SET message = '{0}' where name = '{1}'".format(mes,self.bot.appinfo.owner))
-                self.connection.commit()
-                cursor.execute("SELECT name,message,date_create FROM bot where name = '{0}'".format(self.bot.appinfo.owner))
-                sql_test = cursor.fetchall()
-                embed.add_field(name='By', value=sql_test[0][0], inline=True)
-                embed.add_field(name='Time', value=sql_test[0][2], inline=True)
-                embed.add_field(name='Message content', value=sql_test[0][1], inline=False)
-                await message.channel.send(self.bot.appinfo.owner, embed=embed)
 
 bot = Bot()
 bot.run(bot.token_bot)
---
+++ sqls.py
@@ -0,0 +1,79 @@
+from mysql.connector import connect, Error
+
+class Sql:
+    def connect_sql(self):
+        try:
+            self.connection = connect(
+                host="localhost",
+                user="",
+                password="",
+                database="task", )
+
+
+        except Error as e:
+            print(e)
+    async def getsql(self,message,discord,bot):
+        self.connect_sql()
+        mes = message.content.replace("!enter notes", " ")
+        with self.connection.cursor() as cursor:
+
+              cursor.execute("SELECT id FROM bot where name = '{0}'".format(bot.appinfo.owner))
+              ready_id = 0
+              sql_test = cursor.fetchall()
+              for sql_value in sql_test:
+                  ready_id = sql_value
+
+              if ready_id == 0:
+                    cursor.execute("INSERT INTO bot (name,message) VALUES ('{0}','{1}');".format(bot.appinfo.owner,mes))
+                    self.connection.commit()
+                    await message.channel.send("ваша заметка создана - {0} ".format(mes))
+              else:
+                  await message.channel.send("у вас уже есть заметки, чтобы их вывести введите - !display notes")
+
+    async def setsql(self,message,discord,bot):
+        self.connect_sql()
+        #mes = message.content.replace("!display notes", " ")
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT name,message,date_create FROM bot where name = '{0}'".format(bot.appinfo.owner))
+            sql_test = cursor.fetchall()
+            if not sql_test:
+                await message.channel.send("у вас нету заметок")
+            else:
+
+                embed = discord.Embed(color=0x7daeff)
+                embed.add_field(name='By', value=sql_test[0][0], inline=True)
+                embed.add_field(name='Time', value=sql_test[0][2], inline=True)
+                embed.add_field(name='Message content', value=sql_test[0][1], inline=False)
+                await message.channel.send(bot.appinfo.owner, embed=embed)
+
+
+
+    async def deletesql(self,message,discord,bot):
+        self.connect_sql()
+        with self.connection.cursor() as cursor:
+            cursor.execute("DELETE FROM bot WHERE name = '{0}'".format(bot.appinfo.owner))
+            self.connection.commit()
+            await message.channel.send("Заметки удалены!")
+    async def updatesql(self,message,discord,bot):
+        self.connect_sql()
+        mes = message.content.replace("!update notes", " ")
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT name,message,date_create FROM bot where name = '{0}'".format(bot.appinfo.owner))
+            sql_test = cursor.fetchall()
+            if not sql_test:
+                await message.channel.send("у вас нету заметок для обновления")
+            else:
+
+                embed = discord.Embed(color=0x7daeff)
+                embed.add_field(name='By', value=sql_test[0][0], inline=True)
+                embed.add_field(name='Time', value=sql_test[0][2], inline=True)
+                embed.add_field(name='Message content', value=sql_test[0][1], inline=False)
+                await message.channel.send(bot.appinfo.owner, embed=embed)
+                cursor.execute("UPDATE bot SET message = '{0}' where name = '{1}'".format(mes,bot.appinfo.owner))
+                self.connection.commit()
+                cursor.execute("SELECT name,message,date_create FROM bot where name = '{0}'".format(bot.appinfo.owner))
+                sql_test = cursor.fetchall()
+                embed.add_field(name='By', value=sql_test[0][0], inline=True)
+                embed.add_field(name='Time', value=sql_test[0][2], inline=True)
+                embed.add_field(name='Message content', value=sql_test[0][1], inline=False)
+                await message.channel.send(bot.appinfo.owner, embed=embed)
